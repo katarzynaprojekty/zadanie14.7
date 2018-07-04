c5b32b719b9bbe1f12c805fcd275ee350865f381
Katarzyna Nieszporek <katarzyna.n.projekty@gmail.com>
2018-07-04 14:39:29 +0200

    change ContactForm.js

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -8,39 +8,50 @@ var ContactForm = React.createClass({
         this.props.onFormSubmit(this.state)
     },
 
-    // getInitialState: function() {
-    //     return {
-    //         contacts: {
-    //             firstName: 'a',
-    //             lastName: 'b',
-    //             email: 'c'
-    //         },
-    //     }
-    // },
-
-    change: function(event) {
-        console.log("test");
+    getInitialState: function() {
+        return {
+            firstName: '',
+            lastName: '',
+            email: ''
+        }
     },
+
+    handleChange: function(e) {
+        e.preventDefault();
+        console.log("zmiana");
+        const target = e.nativeEvent.target;
+        const field = target.name;
+        const value = target.value;
+      
+        this.setState({
+          [field]: value
+        })
+      },
  
     render: function() {
+        console.log("dodawanie kontaktu");
         return (
             <form className= {'contactForm'} onSubmit={this.handleFormSubmit}>
                 <input 
                     type='text' 
+                    name='firstName'
                     placeholder='Imię' 
-                    value={this.firstName} 
-                    onChange={this.change('firstName')}
+                    value={this.state.firstName} 
+                    onChange={this.handleChange}
                 />
                 <input 
                     type='text' 
+                    name='lastName'
                     placeholder='Nazwisko' 
-                    value={this.lastName} 
-                    onChange={this.change('lastName')}
+                    value={this.state.lastName} 
+                    onChange={this.handleChange}
                 />
                 <input 
-                    type='email' 
+                    type='email'
+                    name='email' 
                     placeholder='Email' 
-                    value={this.email} 
+                    value={this.state.email} 
+                    onChange={this.handleChange}
                 />
                 <button type='submit'> Dodaj kontakt </button>
             </form>
